a5996d21ac7351219d5ccd5d7d85a1eaf2a977f0
rlb61 <rlb61@duke.edu>
2018-12-18 18:19:22 -0500

    first draft of refactoring, untested

--- data/clean_data.py
+++ data/clean_data.py
@@ -5,6 +5,10 @@ import copy
 import pandas as pd
 import numpy as np
 
+AMINO_ACIDS = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
+                    'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', 'X']
+#X is not really an amino acid; it's a stop codon
+
 class BareGene(object):
     def __init__(self, gene_name):
         """Return one dataframe with the cleaned genetic data and the labels.
@@ -16,7 +20,7 @@ class BareGene(object):
         Also save a dataframe as <gene_name>_data_cleaning_history.csv that documents
         what was removed and why."""
         self.gene_name = gene_name
-        self.obtain_reference_sequence() #creates self.geneseq
+        self.obtain_reference_sequence() #creates self.reference_df
         self.history = pd.DataFrame(np.empty(100000,5),dtype=str), columns=['Healthy_or_Path','Consensus','Position','Change','Reason_Removed'])
         self.history_idx = 0
         
@@ -28,69 +32,112 @@ class BareGene(object):
         self.diseased_original = copy.deepcopy(self.diseased)
         
         #Clean the data
-        for df in [self.healthy, self.diseased]:
-            df = self.remove_disallowed_symbols(df)
-            df = self.remove_consensus_equals_change(df)
-            df = self.remove_consensus_disagrees_with_reference(df)
-            df = self.remove_per_file_dups(df)
+        dfs = {'healthy':self.healthy, 'diseased':self.diseased}
+        for key in dfs.keys():
+            df = dfs[key]
+            print('Working on',key)
+            df = self.remove_disallowed_symbols(df, key)
+            df = self.remove_consensus_equals_change(df, key)
+            df = self.remove_consensus_disagrees_with_reference(df, key)
+            df = self.remove_dups(df, key, 'duplicate_within_file')
         
         #Merge healthy and diseased
-        self.all = self.merge_healthy_and_diseased()
+        merged = self.merge_healthy_and_diseased()
+        merged = self.remove_dups(merged, 'merged','duplicate_across_files')
         
-        #remove things listed as both healthy and pathologic
-        self.all = self.all.drop_duplicates(subset=['Consensus','Position','Change'],
-                                            keep=False)
+        #Save history
+        self.history.to_csv(self.gene_name+'_data_cleaning_history.csv')
+        
+        #TODO write function that checks that everything listed in self.history
+        #is present in the original files and is absent from the current version
+        #of the file.
+        
+        #Done
         return self.all
     
-    def obtain_reference_sequence(self):
-        geneseq = ''
-        with open(os.path.join(self.gene_name,self.gene_name+'_reference_sequence.txt', 'r') as f:
-            for line in f:
-                geneseq = geneseq + line.rstrip()
-        if self.gene_name == 'ryr2':
-            assert len(geneseq) == 4967
-        elif self.gene_name == 'scn5a':
-            pass #TODO (for scn5a and all other genes)
-        self.geneseq = geneseq.upper()
-    
-    def remove_disallowed_symbols(self, df):
+    ####################
+    # Cleaning Methods #--------------------------------------------------------
+    ####################
+    def remove_missing_values(self, df, key):
+        """Remove rows with any missing values"""
+        print('remove_missing_values()\n\tRows before:',df.shape[0])
+        temp = df[df.isnull().any(axis=1)]
+        self.update_history_using_temp(temp, key, 'missing_values')
+        df = df.dropna(axis='index', how='any')
+        print('\tRows after:',df.shape[0])
         return df
     
-    def remove_consensus_equals_change(self, df):
+    def clean_up_symbols(self, df, key):
+        """Clean up symbols so that everything in the Consensus and Change columns
+        corresponds to a letter in AMINO_ACIDS, or is:
+            fs (frameshift)
+            dup (duplication)
+            empty (nothing there, corresponds to symbol -)
+            misc (any other combination of symbols including multiple AAs)"""
+        print('clean_up_symbols()')
+        global AMINO_ACIDS
+        #Just print off when you make a change what that change is
+        for choice in ['Consensus','Change']:
+            for rowidx in df.index.values:
+                current_aa = df.at[rowidx,choice]
+                if current_aa not in AMINO_ACIDS:
+                    if current_aa not in ['dup','fs']:
+                        #Then it's a weird aa and must be cleaned
+                        if 'dup' in current_aa:
+                            print('\t[',rowidx,',',choice,']',current_aa,'-->','dup')
+                            df.at[rowidx,choice] = 'dup'
+                        elif 'fs' in current_aa:
+                            print('\t[',rowidx,',',choice,']',current_aa,'-->','fs')
+                            df.at[rowidx,choice] = 'fs'
+                        elif '-' == current_aa:
+                            print('\t[',rowidx,',',choice,']',current_aa,'-->','empty')
+                            df.at[rowidx,choice] = 'empty'
+                        else:
+                            print('\t[',rowidx,',',choice,']',current_aa,'-->','misc')
+                            df.at[rowidx,choice] = 'misc'
+        return df
+    
+    def remove_consensus_equals_change(self, df, key):
         """Remove anything that is not a real mutation (i.e. the change is the
         same as the consensus sequence)"""
+        #Document history of upcoming change
+        temp = df.loc[df['Consensus']==df['Change']]
+        self.update_history_using_temp(temp, key, 'consensus_equals_change')
+        
+        #Perform change
         print('remove_consensus_equals_change():\n\tRows before:', df.shape[0])
         df = df.loc[df['Consensus']!=df['Change']]
-        print('\tRows after:',healthy.shape[0])
+        print('\tRows after:',df.shape[0])
+        return df
     
-    def remove_consensus_disagrees_with_reference(self):
+    def remove_consensus_disagrees_with_reference(self, df, key):
         """Remove anything where the consensus amino acid disagrees with the
-        ground truth reference amino acid for that position.
-        Note that 'healthy' and 'diseased' are one-based. So position 5 in self.healthy
-        should be referenced as position 4 in self.geneseq (the reference
-        sequence)"""
-        reference_df = pd.DataFrame(np.transpose(np.array([list(self.geneseq), [x for x in range(1,len(self.geneseq)+1)]])),
-                                    columns=['Reference','Position'])
-        #pos columns must have same type in order to be merged
-        reference_df['Position'] = pd.to_numeric(reference_df['Position'], downcast='integer')
-        self.healthy['Position'] = pd.to_numeric(healthy['Position'], downcast='integer')
-        self.diseased['Position'] = pd.to_numeric(diseased['Position'], downcast='integer')
+        ground truth reference amino acid for that position."""
+        #Prepare for the removal
+        print('remove_consensus_disagrees_with_reference()')
+        reference_df = copy.deepcopy(self.reference_df)
+        df['Position'] = pd.to_numeric(df['Position'], downcast='integer')
+        df = df.merge(copy.deepcopy(reference_df), how = 'left', on='Position')
         
-        self.healthy = self.healthy.merge(copy.deepcopy(reference_df), how = 'left', on='Position')
-        self.healthy = self.healthy.loc[healthy['Consensus']==healthy['Reference']]
-        self.diseased = self.diseased.merge(copy.deepcopy(reference_df), how = 'left', on = 'Position')
-        self.diseased = self.diseased.loc[diseased['Consensus']==diseased['Reference']]
-        print('Healthy rows after removing self.healthy entries where consensus!=reference:',healthy.shape[0])
-        print('Diseased rows after removing self.diseased entries where consensus!=reference:',diseased.shape[0])
+        #Document history of upcoming change
+        temp =  df.loc[df['Consensus']!=df['Reference']]
+        self.update_history_using_temp(temp, key, 'consensus_disagrees_w_reference')
+        
+        #Perform change
+        df = df.loc[df['Consensus']==df['Reference']]
+        print('\tRows after:',df.shape[0])
+        return df
     
-    def remove_per_file_dups(self):
-        """Remove any duplicates within a file"""
-        print('Healthy rows before removing self.healthy dups:',healthy.shape[0])
-        print('Diseased rows before removing self.diseased dups:',diseased.shape[0])
-        self.healthy = self.healthy.drop_duplicates(keep = 'first')
-        disease = self.diseased.drop_duplicates(keep = 'first')
-        print('Healthy rows after removing self.healthy dups:',healthy.shape[0])
-        print('Diseased rows after removing self.diseased dups:',diseased.shape[0])        
+    def remove_dups(self, df, key, reason_removed):
+        """Remove duplicates"""
+        print('remove_dups()')
+        #Document history of upcoming change
+        temp = df[df.duplicated(subset=['Consensus','Position','Change'],keep='first')]
+        self.update_history_using_temp(temp, key, reason_removed)
+        
+        #Perform change
+        df = df.drop_duplicates(keep = 'first')
+        print('\tRows after:',df.shape[0])
     
     def merge_healthy_and_diseased(self):
         """Return 'inputx' dataframe for self.healthy and self.diseased"""
@@ -104,6 +151,38 @@ class BareGene(object):
         self.diseased.index = range(helen,helen+dislen)
         return pd.concat([self.healthy,self.diseased])
 
+    ##################
+    # Helper Methods #----------------------------------------------------------
+    ##################
+    def obtain_reference_sequence(self):
+        geneseq = ''
+        with open(os.path.join(self.gene_name,self.gene_name+'_reference_sequence.txt', 'r') as f:
+            for line in f:
+                geneseq = geneseq + line.rstrip()
+        if self.gene_name == 'ryr2':
+            assert len(geneseq) == 4967
+        elif self.gene_name == 'scn5a':
+            pass #TODO (for scn5a and all other genes)
+        geneseq = geneseq.upper()
+        #In this df we create the index starting from 1 because 'healthy' and
+        #'diseased' use one-based positions and we must be consistent.
+        reference_df = pd.DataFrame(np.transpose(np.array([list(geneseq), [x for x in range(1,len(geneseq)+1)]])),
+                                    columns=['Reference','Position'])
+        #pos columns must have same type in order to be merged with other df later
+        reference_df['Position'] = pd.to_numeric(reference_df['Position'], downcast='integer')
+        self.reference_df = reference_df
+
+    def update_history_using_temp(self, temp, key, reason_removed):
+        """Update history of the data cleaning process"""
+        for rowidx in temp.index.values:
+            self.history.at[self.history_idx,'Healthy_or_Path'] = key
+            self.history.at[self.history_idx,'Consensus'] = temp.at[rowidx,'Consensus']
+            self.history.at[self.history_idx,'Position'] = temp.at[rowidx,'Position']
+            self.history.at[self.history_idx,'Change'] = temp.at[rowidx,'Change']
+            if reason_removed == 'consensus_disagrees_w_reference':
+                reason_removed += ': '+temp.at[rowidx,'Reference']
+            self.history.at[self.history_idx,'Reason_Removed'] = reason_removed
+            self.history_idx += 1
 
 class AnnotatedGene(object):
     def __init__(self, gene_name):
@@ -162,6 +241,7 @@ class AnnotatedGene(object):
 def prepare_everyAA(real_data):
     """Create a pandas dataframe containing every possible amino acid change
     at every position, except for anything present in the real data."""
+    global AMINO_ACIDS
     print('Creating everyAA')
     #Read in the PreparedGeneticData sequence
     geneseq = ''
@@ -172,9 +252,7 @@ def prepare_everyAA(real_data):
     geneseq = geneseq.upper()
     
     #List of all possible AAs:
-    possible_aas = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
-                    'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
-    max_length = len(geneseq)*(len(possible_aas)-1) #don't count C-->C (not a change)
+    max_length = len(geneseq)*(len(AMINO_ACIDS)-1) #don't count C-->C (not a change)
     print('everyAA max length is',max_length)
     
     #Fill dataframe
@@ -182,7 +260,7 @@ def prepare_everyAA(real_data):
     dfindex = 0
     for position in range(len(geneseq)):
         consensus = geneseq[position]
-        for aa in possible_aas:
+        for aa in AMINO_ACIDS:
             if aa != consensus: 
                 everyAA.at[dfindex, 'Position'] = str(position+1) #one-based
                 everyAA.at[dfindex, 'Consensus'] = consensus
@@ -209,9 +287,9 @@ def prepare_everyAA(real_data):
 
 def make_small_everyAA_for_testing():
     """Dummy version of everyAA to be able to run the MLP code quickly"""
-    possible_aas = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
+    AMINO_ACIDS = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
                     'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
-    everyAA = pd.DataFrame(np.transpose(np.array([possible_aas, possible_aas])),
+    everyAA = pd.DataFrame(np.transpose(np.array([AMINO_ACIDS, AMINO_ACIDS])),
                         columns=['Consensus','Change'])
     everyAA['Label'] = 0
     everyAA['Position'] = [x for x in range(1,21)]
