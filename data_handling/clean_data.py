7492d158034d8566d233a97b1eb6ef0cfdeb8178
rlb61 <rlb61@duke.edu>
2018-12-17 19:02:07 -0500

    reorganization

---
+++ data_handling/clean_data.py
@@ -0,0 +1,101 @@
+#Rachel Ballantyne Draelos
+
+import copy
+import pandas as pd
+import numpy as np
+
+class CleanGenetics(object):
+    """Return two dataframes, healthy and pathologic, which contain the cleaned
+    genetic data. Furthermore, create an output file 'data_cleaning_history.csv'
+    which documents what was filtered out of the original data, and why."""
+    def __init__(self, healthy_file, pathologic_file):
+        """<healthy_file> and <pathologic_file> are paths to CSVs with columns
+        ['Consensus','Position','Change']   """
+        
+        pass
+    
+    
+
+
+def remove_wrong():
+    healthy = pd.read_csv('/data/rlb61/Collaborations/Landstrom/datafiles/Healthy_Variants_deduped.csv', header=0)
+    diseased = pd.read_csv('/data/rlb61/Collaborations/Landstrom/datafiles/Pathologic_Variants_deduped.csv', header=0)
+    
+    #reference seq
+    ryr2seq = ''
+    with open('/data/rlb61/Collaborations/Landstrom/datafiles/RyR2_Protein_Sequence_Human.txt','r') as f:
+        for line in f:
+            ryr2seq = ryr2seq + line.rstrip()
+    assert len(ryr2seq) == 4967
+    ryr2seq = ryr2seq.upper()
+    
+    #Remove anything that is not a real mutation (i.e. the "change" is the consensus sequence)
+    print('Healthy rows before removing healthy entries where consensus==change:',healthy.shape[0])
+    print('Diseased rows before removing diseased entries where consensus==change:',diseased.shape[0])
+    healthy = healthy.loc[healthy['Consensus']!=healthy['Change']]
+    diseased = diseased.loc[diseased['Consensus']!=diseased['Change']]
+    print('Healthy rows after removing healthy entries where consensus==change:',healthy.shape[0])
+    print('Diseased rows after removing diseased entries where consensus==change:',diseased.shape[0])
+    
+    #Now remove anything that disagrees with the consensus sequence
+    #note that 'healthy' and 'diseased' are one-based. So position 5 in healthy
+    #should be referenced as position 4 in the reference sequence
+    reference_df = pd.DataFrame(np.transpose(np.array([list(ryr2seq), [x for x in range(1,len(ryr2seq)+1)]])),
+                                columns=['Reference','Position'])
+    #pos columns must have same type in order to be merged
+    reference_df['Position'] = pd.to_numeric(reference_df['Position'], downcast='integer')
+    healthy['Position'] = pd.to_numeric(healthy['Position'], downcast='integer')
+    diseased['Position'] = pd.to_numeric(diseased['Position'], downcast='integer')
+    
+    healthy = healthy.merge(copy.deepcopy(reference_df), how = 'left', on='Position')
+    healthy = healthy.loc[healthy['Consensus']==healthy['Reference']]
+    diseased = diseased.merge(copy.deepcopy(reference_df), how = 'left', on = 'Position')
+    diseased = diseased.loc[diseased['Consensus']==diseased['Reference']]
+    print('Healthy rows after removing healthy entries where consensus!=reference:',healthy.shape[0])
+    print('Diseased rows after removing diseased entries where consensus!=reference:',diseased.shape[0])
+    
+    healthy[['Position','Consensus','Change']].to_csv('Healthy_Variants_repaired.csv', header=True, index=False)
+    diseased[['Position','Consensus','Change']].to_csv('Pathologic_Variants_repaired.csv', header=True, index=False)
+    print('Done')
+    
+if __name__=='__main__':
+    remove_wrong()
+
+
+def remove_dups():
+    healthy = pd.read_csv('/data/rlb61/Collaborations/Landstrom/datafiles/Healthy_Variants.csv', header=0)
+    diseased = pd.read_csv('/data/rlb61/Collaborations/Landstrom/datafiles/Pathologic_Variants.csv', header=0)
+    
+    #Remove any file-specific dups
+    print('Healthy rows before removing healthy dups:',healthy.shape[0])
+    print('Diseased rows before removing diseased dups:',diseased.shape[0])
+    healthy = healthy.drop_duplicates(keep = 'first')
+    disease = diseased.drop_duplicates(keep = 'first')
+    print('Healthy rows after removing healthy dups:',healthy.shape[0])
+    print('Diseased rows after removing diseased dups:',diseased.shape[0])
+    
+    #Now remove cross-file dups (i.e. example that appear in each file)
+    #I got these dups using unix
+    #first force them to be a dup in the individual file, and then remove
+    #all dups from the individual file
+    print('Healthy rows after removing cross-file dups should be',healthy.shape[0]-25)
+    print('Diseased rows after removing cross-file dups should be',diseased.shape[0]-25)
+    dups = pd.DataFrame([[1013,'R','Q'], [1107,'T','M'], [186,'V','M'], [2113,'V','M'],
+        [2145,'G','R'], [217,'I','V'], [2267,'R','H'], [2359,'R','Q'],
+        [2392,'Y','C'], [240,'H','R'], [2487,'L','I'], [2504,'T','M'],
+        [2510,'T','A'], [3800,'C','F'], [420,'R','W'], [4282,'A','V'],
+        [4307,'R','C'], [4552,'H','R'], [4556,'A','T'], [4662,'G','S'],
+        [466,'P','A'], [4790,'R','Q'], [62,'L','F'], [739,'R','H'],
+        [77,'A','V']], columns = ['Position','Consensus','Change'])
+    healthy = pd.concat([healthy,copy.deepcopy(dups)], axis = 0)
+    healthy = healthy.drop_duplicates(keep = False)
+    diseased = pd.concat([diseased, copy.deepcopy(dups)], axis = 0)
+    diseased = diseased.drop_duplicates(keep = False)
+    print('Healthy rows after removing healthy cross-file dups:',healthy.shape[0])
+    print('Diseased rows after removing diseased cross-file dups:',diseased.shape[0])
+    healthy.to_csv('Healthy_Variants_deduped.csv', header=True, index=False)
+    diseased.to_csv('Pathologic_Variants_deduped.csv', header=True, index=False)
+    print('Done')
+    
+if __name__=='__main__':
+    remove_dups()
